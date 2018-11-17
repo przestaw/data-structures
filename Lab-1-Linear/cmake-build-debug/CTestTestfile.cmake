6c19ac48bb02b318596eca153cbe8247ff157400
przestaw <pstawczy@mion.elka.pw.edu.pl>
2018-11-18 00:21:36 +0100

    partial implementation in Lab1

--- Lab-1-Linear/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/przemek/AISDI/Lab-1-Linear
-# Build directory: /home/przemek/AISDI/Lab-1-Linear/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- Lab-1-Linear/cmake-build-debug/src/CMakeFiles/aisdiLinear.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for aisdiLinear.
-# This may be replaced when dependencies are built.
